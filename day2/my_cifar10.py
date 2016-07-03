15fcfd9c0a60a010bfabce3ac9d872c2255620b6
Martyn Bristow <martyn.bristow@gmail.com>
2016-07-03 11:02:37 +0100

    day1 update

---
+++ day2/my_cifar10.py
@@ -0,0 +1,85 @@
+from __future__ import absolute_import
+from keras.datasets.cifar import load_batch
+from keras.utils.data_utils import get_file
+import numpy as np
+import os
+
+def get_file(fname, origin, untar=False):
+    datadir_base = os.path.expanduser(os.path.join('~', '.keras'))
+    if not os.access(datadir_base, os.W_OK):
+        datadir_base = os.path.join('/tmp', '.keras')
+    datadir = os.path.join(datadir_base, 'datasets')
+    if not os.path.exists(datadir):
+        os.makedirs(datadir)
+
+    if untar:
+        untar_fpath = os.path.join(datadir, fname)
+        fpath = untar_fpath + '.tar.gz'
+    else:
+        fpath = os.path.join(datadir, fname)
+
+    if not os.path.exists(fpath):
+        print('Downloading data from',  origin)
+        global progbar
+        progbar = None
+
+        def dl_progress(count, block_size, total_size):
+            global progbar
+            if progbar is None:
+                progbar = Progbar(total_size)
+            else:
+                progbar.update(count*block_size)
+
+        error_msg = 'URL fetch failure on {}: {} -- {}'
+        try:
+            try:
+                urlretrieve(origin, fpath, dl_progress)
+            except URLError as e:
+                raise Exception(error_msg.format(origin, e.errno, e.reason))
+            except HTTPError as e:
+                raise Exception(error_msg.format(origin, e.code, e.msg))
+        except (Exception, KeyboardInterrupt) as e:
+            if os.path.exists(fpath):
+                os.remove(fpath)
+            raise
+        progbar = None
+
+    if untar:
+        if not os.path.exists(untar_fpath):
+            print('Untaring file...')
+            tfile = tarfile.open(fpath, 'r:gz')
+            try:
+                tfile.extractall(path=datadir)
+            except (Exception, KeyboardInterrupt) as e:
+                if os.path.exists(untar_fpath):
+                    if os.path.isfile(untar_fpath):
+                        os.remove(untar_fpath)
+                    else:
+                        shutil.rmtree(untar_fpath)
+                raise
+            tfile.close()
+        return untar_fpath
+
+    return fpath
+
+def load_data():
+    dirname = "cifar-10-batches-py"
+
+    nb_train_samples = 50000
+
+    X_train = np.zeros((nb_train_samples, 3, 32, 32), dtype="uint8")
+    y_train = np.zeros((nb_train_samples,), dtype="uint8")
+
+    for i in range(1, 6):
+        fpath = os.path.join('/day2', dirname, 'data_batch_' + str(i))
+        data, labels = load_batch(fpath)
+        X_train[(i-1)*10000:i*10000, :, :, :] = data
+        y_train[(i-1)*10000:i*10000] = labels
+
+    fpath = os.path.join('/day2', dirname, 'test_batch')
+    X_test, y_test = load_batch(fpath)
+
+    y_train = np.reshape(y_train, (len(y_train), 1))
+    y_test = np.reshape(y_test, (len(y_test), 1))
+
+    return (X_train, y_train), (X_test, y_test)
